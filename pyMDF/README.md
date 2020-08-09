a34a25c4fd57500e99a31dda550f9c1858df2972
Ryan Quey <quey.ryan@gmail.com>
2020-08-08 22:39:52 -0700

    cleanup python code; add more comments and documentation

---
+++ pyMDF/README.md
@@ -0,0 +1,5 @@
+# What is pyMDF?
+pyMDF is a python implementation of mdf. This allows reading and writing the same data that mMdf does, but in the python language. Data stored by mMdf in your db can be read by pyMDF and vice versa.
+
+# Setup 
+Just run `./setup.sh`. This will open a jupyter lab notebook for you, which walks you through a tutorial to MDF.
--- pyMDF/scripts/setup.sh
+++ pyMDF/scripts/setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# TODO move this into the root project's scripts dir, so it's all in one place. Or alternatively, move those scripts into mMdf so stuff is kept separate.
+
 # make sure we are in the current folder so venv relative paths work
-cd "$(dirname "$0")"
+cd "$(dirname "$0")/.."
 
 # setup venv
 # NOTE if ran from this script, doesn't activate in the terminal you called this script from
@@ -11,8 +13,8 @@ python3 -m venv venv && \
 # install Python dependencies
 pip3 install wheel
 pip3 install -r ./requirements.txt
-# these libs are only strictly necessary for 
 pip3 install -r ./mdf_playground/requirements.txt
 
 # start the notebook 
+# TODO add an option so that this isn't started. Or better yet, maybe split it out into a separate script; one for setup, one for running
 python3 -m jupyter lab
